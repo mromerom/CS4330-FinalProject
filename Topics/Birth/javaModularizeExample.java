62975499a781a93e5dcb1b142dc027563e617445
pdogrugger <pmhkv9@mail.missouri.edu>
2017-04-25 00:43:10 -0500

    Update javaModularizeExample.java

--- Topics/Birth/javaModularizeExample.java
+++ Topics/Birth/javaModularizeExample.java
@@ -1,3 +1,4 @@
+// Option 1
 double a = -16.00;
 double b = 100.00;
 double c = 6.00
@@ -10,13 +11,7 @@ else
    double ans2 = (-b + sqrt(determinant))/(2*a);
    print("x = " + ans1 + " or " x = " + ans2 );
 
-// the above code is fine when you're not using the quadratic equation multiple times
-// but imagine many different people needing to use this; wouldn't it be easier to
-//  make a copy of the option where people just send their numbers in and don't have to
-//  worry about creating the function themselves or taking this one and re-writing the
-//  input
-//  it would be much easier to call a function that does it for you
-
+// Option 2
 quadraticEquation(a, b, c);
 
 public void quadraticEquation(double a, double b, double c)
@@ -29,8 +24,15 @@ public void quadraticEquation(double a, double b, double c)
       double ans2 = (-b + sqrt(determinant))/(2*a);
       print("x = " + ans1 + " or " x = " + ans2 );
 }
-
-/* this version is much better and less wasting of time, as it is easily
+ 
+/* 
+   option 1 is fine when you're not using the quadratic equation multiple times
+   but imagine many different people needing to use this; wouldn't it be easier to
+   make a copy of the option where people just send their numbers in and don't have to
+   worry about creating the function themselves or taking this one and re-writing the
+   input
+   it would be much easier to call a function that does it for you
+   option 2 is much better and less wasting of time, as it is easily
       used by those who can know the name of the function and its parameters
       (what to send the function)
 *\
